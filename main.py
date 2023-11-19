99d2ecf8f5dbbe40944e3d341e76d9979b3a94e4
valentin <v.prokhorov@telecontact.ru>
2023-11-19 16:03:52 +0300

    (fix)мелкие правки

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-
 import test_categories_jokes
 import receiving_data_user
-
 
 testApi = test_categories_jokes.TestJoke()
 categories = testApi.get_all_categories()
 jokes = testApi.get_jokes_from_all_categories()
 question = receiving_data_user.GetJokesByUser().question()
-
