c177f3cb963bff0a1d9330efaed6913e03e7f38b
valentin <v.prokhorov@telecontact.ru>
2023-11-22 17:52:27 +0300

    (fix) мелкие правки

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import test_categories_jokes
 import receiving_data_user
 import place
 
-# testApi = test_categories_jokes.TestJoke()
-# categories = testApi.get_all_categories()
-# jokes = testApi.get_jokes_from_all_categories()
-# question = receiving_data_user.GetJokesByUser().question()
+testApi = test_categories_jokes.TestJoke()
+categories = testApi.get_all_categories()
+jokes = testApi.get_jokes_from_all_categories()
+question = receiving_data_user.GetJokesByUser().question()
 testLocation = place.Test_new_location()
-# # getLocation = place.GetLocation()
-# testLocation.test_create_locations()
+testLocation.test_create_locations()
 testLocation.get_location()
